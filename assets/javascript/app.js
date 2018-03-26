b4f33e895a1dfc327c251a4046bfbf8f5c862996
Waleed Hamza <waleedhamza@Waleeds-MacBook-Pro.local>
2018-03-26 00:19:29 -0400

    html+css finished+ added some functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,32 @@
-$("#start").click(function(startGame){
-$("#x").hide("#x");
-console.log(startGame);
+var startGame;
+
+//start the Game function 
+$("#start").click(function(startGame) {
+$("#intro").hide("#intro");
+$("#action").show("#action");
+
+//count down timer // 
+var windowTimeout = setTimeout(function() {
+  $("#overlay").show("#overlay");
+      }, 60000);
 });
 
-startGame();
+// add on click event to reset the game // 
+$("#overlay").click(function (){
+  $("#into").show$("#into");
+});
+
+var currentQuestion;
+
+//random question generator// 
+function getRandomQuestion (){
+  var random = Math.floor(Math.random()* questions.length);
+  return questions[random];
+
+} 
+
+//Get answers from the the question variable questions.answer//
+function getAnswers(questions) {
+  var answers = [];
+  answers.push(questions.answer);
+}
---
+++ assets/javascript/questions.js
@@ -0,0 +1,42 @@
+var questions=[
+  {
+  question: "What was the first branch of the military to officially purchase AR-15-type rifles?"
+  ,answear: ["√Air Force" , "Army", "Navy", "Marines"]
+},
+
+{
+  question: "Using a silencer on an AR-15 rifle will change your bullet’s point of impact which way?"
+  ,answear: [ "To the left and upward","To the left and downward","Downward","√Randomly or perhaps not at all"]
+},
+
+{
+  question: "If subsonic, 55-grain loads are fired from a 16-inch rifle barrel, the AR-15’s muzzle energy goes from approximately 1280 ft.lbs. to what?"
+  ,answear: [ "1000", "500", "√125", "There is no change in muzzle energy"]
+},
+
+{
+  question: "AR, as in AR-15, stands for?"
+  ,answear: ["Eugene ARnold Stone", "√ArmaLite Rifle", "Assault Rifle", "none of the above"]
+},
+
+{
+question: "Approximately how many ft.-lbs. of recoil energy does a standard-velocity, .223 Rem/5.56 NATO cartridge loaded with a 55-grain bullet produce in a typical, 7-pound AR-15 rifle?"
+  ,answear: ["17.5", "√3.7", "8.1", ".75"]
+},
+
+{
+question: "The National Shooting Sports Foundation estimates there are how many “Modern Sporting Arms (AR-15-style rifles) in the U.S. today?"
+  ,answear: ["Fewer than 1 million", "300 million", "√5 million to 9 million", "25-35 million"]
+},
+
+{
+question: "An M4 carbine differs from the military’s M16 rifle, which was adapted from the AR-15, in what way?"
+  ,answear: ["It’s a fully automatic version", "It’s chambered in .7.62 NATO", "√It’s shorter and lighter", "It’s the exact same rifle, just coded differently by various military branches"]
+},
+
+{
+question: "Which of the following rifles is NOT an AR-15-style rifle?"
+  ,answear: ["√Keltec RDB", "Rock River LAR 15", "Daniel Defense MK 18", "Yankee Hill Machine 8550 Specter"]
+}
+]
+
