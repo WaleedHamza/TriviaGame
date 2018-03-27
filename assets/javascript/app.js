2ad9a14f0511d423638d163f4d72241418ed32dd
Waleed Hamza <waleedhamza@Waleeds-MacBook-Pro.local>
2018-03-27 18:43:28 -0400

    value checker added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,53 +4,49 @@ var startGame;
 $("#start").click(function(startGame) {
 $("#intro").hide("#intro");
 $("#action").show("#action");
-
+});
 //count down timer // 
 var windowTimeout = setTimeout(function() {
   $("#overlay").show("#overlay");
       }, 120000);
-});
 
-// // add on click event to reset the game // 
-// $("#restart").click(function (){
-//   $("#overlay").hide("#overlay");
-//   $("#action").hide("#action");
-//   $("#intro").show("#intro");
-// });
+
+// add on click event to reset the game // 
+$("#restart").click(function (){
+  $("#overlay").hide("#overlay");
+  $("#action").hide("#action");
+  $("#intro").show("#intro");
+});
 
 var numOfQuestions = 8;
 var score = 0;
-
+var wrongAnswers = []
 // capture input//
-var q1 = $(".q1").click(function(){
-        alert($(this).val());
-})
+var optionsInPutQ1 = $("#btn1, #btn2, #btn3, #btn4").click(function(event){
+  event.preventDefault();
+  //setting a var to a true false
+  var notCorrect = false
+  var missed = $(this).attr("class")
+  console.log(missed)
+  //for loop through wrong answers and change var if its int here
+  for (var i = 0 ; i < wrongAnswers.length; i++ ){
+    //value checker & prevent repeated clicks //
+    if (missed ==  wrongAnswers[i] ){
+      notCorrect = true
+    }
+  }
+  //if inside for
+  var guess = $(this).val();
+   if (guess == "true" && !notCorrect ) {
+     score++
+     $("#score").text(score);
+     wrongAnswers.push(missed)
+   }
+});
 
-var q2 = $(".q2").click(function(){
-        alert($(this).val());
-})
 
-var q3 = $(".q3").click(function(){
-        alert($(this).val());
-})
 
-var q4 = $(".q4").click(function(){
-  alert($(this).val());
-})
 
-var q5 = $(".q5").click(function(){
-  alert($(this).val());
-})
 
-var q6 = $(".q6").click(function(){
-  alert($(this).val());
-})
 
-var q7 = $(".q7").click(function(){
-  alert($(this).val());
-})
-
-var q8 = $(".q8").click(function(){
-  alert($(this).val());
-})
-
+$("#score").text(score);
