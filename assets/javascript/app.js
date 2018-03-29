74ecdb6a8b07b0569dbf1147fbd87cb6ffca9648
Waleed Hamza <waleedhamza@Waleeds-MacBook-Pro.local>
2018-03-29 11:43:32 -0400

    All IS WELL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,44 @@
-var startGame;
+var numOfQuestions = 8;
+var wrongAnswers = []
+var score = 0;
 
 //start the Game function 
-$("#start").click(function(startGame) {
+$("#start").click(function() {
 $("#intro").hide("#intro");
 $("#action").show("#action");
-});
 //count down timer // 
 var windowTimeout = setTimeout(function() {
   $("#overlay").show("#overlay");
+   var score = 0;
       }, 120000);
-
+    });
 
 // add on click event to reset the game // 
 $("#restart").click(function (){
-  $("#overlay").hide("#overlay");
-  $("#action").hide("#action");
-  $("#intro").show("#intro");
+  location.reload();
 });
 
-var numOfQuestions = 8;
-var score = 0;
-var wrongAnswers = []
+$("#submit").click(function(){
+  $("#score").append(score)
+  $("#overlay").show("#overlay");
+})
+
+
 // capture input//
 var optionsInPutQ1 = $("#btn1, #btn2, #btn3, #btn4").click(function(event){
   event.preventDefault();
   //setting a var to a true false
   var notCorrect = false
   var missed = $(this).attr("class")
-  console.log(missed)
-  //for loop through wrong answers and change var if its int here
+  // console.log(missed)
+  //for loop through wrong answers and change var if its not here
   for (var i = 0 ; i < wrongAnswers.length; i++ ){
     //value checker & prevent repeated clicks //
     if (missed ==  wrongAnswers[i] ){
-      notCorrect = true
+        notCorrect = true
     }
   }
-  //if inside for
+//score counter//
   var guess = $(this).val();
    if (guess == "true" && !notCorrect ) {
      score++
@@ -44,9 +47,13 @@ var optionsInPutQ1 = $("#btn1, #btn2, #btn3, #btn4").click(function(event){
    }
 });
 
-
-
-
-
-
-$("#score").text(score);
+//when the submit button is clicked display the score div 
+$("#submit").click(function(){
+  $("#score").text(score)
+  $("#score").show ("#score")
+ })
+  
+//display solution div//
+$("#solution").click( function(){
+  $("#solutionTable").show("#solutionTable")
+});
