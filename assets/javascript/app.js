5d9f734197e570917e35355ed60f3a06b1eedcbd
Waleed Hamza <waleedhamza@Waleeds-MacBook-Pro.local>
2018-03-31 16:03:15 -0400

    timer finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,53 @@
-var numOfQuestions = 8;
 var wrongAnswers = []
 var score = 0;
-
+var timeLimit = 120;
+var timeCounter = 0;
+var setInt;
 //start the Game function 
 $("#start").click(function() {
-$("#intro").hide("#intro");
-$("#action").show("#action");
-//count down timer // 
-var windowTimeout = setTimeout(function() {
-  $("#overlay").show("#overlay");
-   var score = 0;
-      }, 120000);
-    });
+
+   setInt = setInterval(countDown, 1000);
+    $("#intro").hide("#intro");
+    $("#action").show("#action");
+    $("#timer").show("#timer");
+    //countdown timer // 
+    var windowTimeout = setTimeout(function() {
+      $("#overlay").show("#overlay");
+      var score = 0;
+          }, 120000);
+});
+
+
+//stopwatch//
+
+
+
+function countDown() {
+  timeLimit--;
+  $("#timer").text(timeLimit);
+  if(timeLimit === 0){
+    console.log("timelimit = 0");
+    clearInterval(setInt)
+    $("#overlay").show("#overlay");
+
+  }
+}
+
+
 
 // add on click event to reset the game // 
 $("#restart").click(function (){
   location.reload();
 });
-
-$("#submit").click(function(){
+function reset() {
   $("#score").append(score)
   $("#overlay").show("#overlay");
+  var setInt = setInterval(countDown, 1000);
+};
+$("#submit").click(function(){
+  clearInterval(setInt);
+  reset();
 })
-
-
 // capture input//
 var optionsInPutQ1 = $("#btn1, #btn2, #btn3, #btn4").click(function(event){
   event.preventDefault();
